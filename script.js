b9067fa14790b5bea4144a5c88bccd54339e02fa
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-05-01 22:46:45 +0100

    Add key to map

--- script.js
+++ script.js
@@ -11,19 +11,45 @@ function initMap() {
   let infoWindows = [];
   let markers = [];
 
-  let area = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
-  let food = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
-  let nightlife = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
-  let landmark = "http://maps.google.com/mapfiles/ms/icons/orange-dot.png";
-  let hotel = "http://maps.google.com/mapfiles/ms/icons/pink-dot.png";
-  let school = "http://maps.google.com/mapfiles/ms/icons/purple-dot.png";
-  let store = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
-  let misc = "http://maps.google.com/mapfiles/ms/icons/ltblue-dot.png";
+  let icons = {
+    area: {
+      icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+      name: "Area",
+    },
+    food: {
+      icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+      name: "Food",
+    },
+    nightlife: {
+      icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+      name: "Nightlife",
+    },
+    landmark: {
+      icon: "http://maps.google.com/mapfiles/ms/icons/orange-dot.png",
+      name: "Landmark",
+    },
+    hotel: {
+      icon: "http://maps.google.com/mapfiles/ms/icons/pink-dot.png",
+      name: "Hotel",
+    },
+    school: {
+      icon: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png",
+      name: "School",
+    },
+    store: {
+      icon: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png",
+      name: "Store",
+    },
+    misc: {
+      icon: "http://maps.google.com/mapfiles/ms/icons/ltblue-dot.png",
+      name: "Misc.",
+    },
+  };
 
   let charming = [
     {
       position: new google.maps.LatLng(34.081792, -118.389374),
-      icon: food,
+      icon: icons.food.icon,
       book: "charming",
       content: `
         <h2>Troubadour</h2>
@@ -35,7 +61,7 @@ function initMap() {
   let slowDays = [
     {
       position: new google.maps.LatLng(33.84383, -118.16778),
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "slowDays",
       content: `
       <h2>Forest Lawn</h2>
@@ -44,7 +70,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(35.37329, -119.01871),
-      icon: area,
+      icon: icons.area.icon,
       book: "slowDays",
       content: `
       <h2>Bakersfield</h2>
@@ -53,7 +79,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.074049, -118.240014),
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "slowDays",
       content: `
       <h2>Dodger Stadium</h2>
@@ -62,7 +88,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.01945, -118.23995),
-      icon: area,
+      icon: icons.area.icon,
       book: "slowDays",
       content: `
       <h2>Santa Monica</h2>
@@ -71,7 +97,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.1044, -118.34322),
-      icon: hotel,
+      icon: icons.hotel.icon,
       book: "slowDays",
       content: `
       <h2>The landmark Motor hotel</h2>
@@ -80,7 +106,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.102132, -118.335441),
-      icon: food,
+      icon: icons.food.icon,
       book: "slowDays",
       content: `
       <h2>Musso and Frank Grill</h2>
@@ -89,7 +115,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.09088, -118.34642),
-      icon: food,
+      icon: icons.food.icon,
       book: "slowDays",
       content: `
       <h2>Ports</h2>
@@ -98,7 +124,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(33.54271, -117.78535),
-      icon: area,
+      icon: icons.area.icon,
       book: "slowDays",
       content: `
       <h2>Laguna</h2>
@@ -107,7 +133,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.10233, -118.29134),
-      icon: food,
+      icon: icons.food.icon,
       book: "slowDays",
       content: `
       <h2>Sarno’s</h2>
@@ -116,7 +142,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(33.83029, -116.54529),
-      icon: area,
+      icon: icons.area.icon,
       book: "slowDays",
       content: `
       <h2>Palm Springs</h2>
@@ -125,7 +151,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(33.5539, -117.80815),
-      icon: area,
+      icon: icons.area.icon,
       book: "slowDays",
       content: `
       <h2>Emerald Bay</h2>
@@ -134,7 +160,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.09728, -118.36633),
-      icon: hotel,
+      icon: icons.hotel.icon,
       book: "slowDays",
       content: `
       <h2>The Garden of Allah</h2>
@@ -143,7 +169,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.10793, -118.27747),
-      icon: school,
+      icon: icons.school.icon,
       book: "slowDays",
       content: `
       <h2>John Marshall High School</h2>
@@ -152,7 +178,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.09916, -118.34005),
-      icon: school,
+      icon: icons.school.icon,
       book: "slowDays",
       content: `
       <h2>Hollywood High School</h2>
@@ -161,7 +187,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.06947, -118.40353),
-      icon: food,
+      icon: icons.food.icon,
       book: "slowDays",
       content: `
       <h2>The Luau</h2>
@@ -170,7 +196,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.08178, -118.38907),
-      icon: food,
+      icon: icons.food.icon,
       book: "slowDays",
       content: `
       <h2>Dan Tana’s</h2>
@@ -179,7 +205,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(33.99361, -118.4799),
-      icon: area,
+      icon: icons.area.icon,
       book: "slowDays",
       content: `
       <h2>Venice Beach</h2>
@@ -188,7 +214,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.09085, -118.37463),
-      icon: food,
+      icon: icons.food.icon,
       book: "slowDays",
       content: `
       <h2>Barney’s Beanery</h2>
@@ -197,7 +223,7 @@ function initMap() {
     },
     {
       position: new google.maps.LatLng(34.098083, -118.368192),
-      icon: hotel,
+      icon: icons.hotel.icon,
       book: "slowDays",
       content: `
       <h2>Chateau Marmont</h2>
@@ -210,7 +236,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.08148, -118.414168),
       map: losAngelesMap,
-      icon: hotel,
+      icon: icons.hotel.icon,
       book: "evesHollywood",
       content: `
       <h2>The Beverly Hills Hotel</h2>
@@ -220,7 +246,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.067265, -118.400225),
       map: losAngelesMap,
-      icon: hotel,
+      icon: icons.hotel.icon,
       book: "evesHollywood",
       content: `
       <h2>The Beverly Wilshire</h2>
@@ -230,7 +256,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.102132, -118.335441),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>Musso and Frank Grill</h2>
@@ -240,7 +266,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.10265, -118.337518),
       map: losAngelesMap,
-      icon: nightlife,
+      icon: icons.nightlife.icon,
       book: "evesHollywood",
       content: `
       <h2>Don the Beachcomber</h2>
@@ -250,7 +276,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.098461, -118.345873),
       map: losAngelesMap,
-      icon: store,
+      icon: icons.store.icon,
       book: "evesHollywood",
       content: `
       <h2> Consumer’s Liquor</h2>
@@ -260,7 +286,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.098083, -118.368192),
       map: losAngelesMap,
-      icon: hotel,
+      icon: icons.hotel.icon,
       book: "evesHollywood",
       content: `
       <h2>Chateau Marmont</h2>
@@ -270,7 +296,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.081846, -118.389471),
       map: losAngelesMap,
-      icon: nightlife,
+      icon: icons.nightlife.icon,
       book: "evesHollywood",
       content: `
       <h2>The Troubadour</h2>
@@ -280,7 +306,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.050808, -118.247879),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Bradbury Building</h2>
@@ -290,7 +316,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.101603, -118.327121),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Broadway Hollywood Building</h2>
@@ -300,7 +326,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.091924, -118.380652),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>Pupi's Combination Bakery and Sidewalk Cafe</h2>
@@ -310,7 +336,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.048, -118.254713),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>Clifton's Cafeteria</h2>
@@ -320,7 +346,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.102352, -118.319354),
       map: losAngelesMap,
-      icon: misc,
+      icon: icons.misc.icon,
       book: "evesHollywood",
       content: `
       <h2>Hawaii Theater</h2>
@@ -330,7 +356,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.06947, -118.40353),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>The Luau</h2>
@@ -340,7 +366,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.09267, -118.37849),
       map: losAngelesMap,
-      icon: nightlife,
+      icon: icons.nightlife.icon,
       book: "evesHollywood",
       content: `
       <h2>The Crescendo</h2>
@@ -350,7 +376,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.10207, -118.32646),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Hollywood & Vine</h2>
@@ -360,7 +386,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.09479, -118.31685),
       map: losAngelesMap,
-      icon: school,
+      icon: icons.school.icon,
       book: "evesHollywood",
       content: `
       <h2>Josheph Le Conte Middle School</h2>
@@ -370,7 +396,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.05134, -118.25021),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Angels Flight Railway</h2>
@@ -380,7 +406,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.08223, -118.31574),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>The Polar Palace</h2>
@@ -390,7 +416,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.44804, -119.24288),
       map: losAngelesMap,
-      icon: area,
+      icon: icons.area.icon,
       book: "evesHollywood",
       content: `
       <h2>Ojai</h2>
@@ -400,7 +426,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.04576, -118.29956),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Santa Sophia Greek Orthodox Cathedral</h2>
@@ -410,7 +436,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.09764, -118.36533),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Schwab’s Pharmacy</h2>
@@ -420,7 +446,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.09031, -118.38335),
       map: losAngelesMap,
-      icon: nightlife,
+      icon: icons.nightlife.icon,
       book: "evesHollywood",
       content: `
       <h2>Café Society</h2>
@@ -430,7 +456,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.09728, -118.36633),
       map: losAngelesMap,
-      icon: hotel,
+      icon: icons.hotel.icon,
       book: "evesHollywood",
       content: `
       <h2>The Garden of Allah</h2>
@@ -440,7 +466,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.07877, -118.3618),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>Cantor’s</h2>
@@ -450,7 +476,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.13655, -118.29419),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Griffith Park</h2>
@@ -460,7 +486,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.09811, -118.34394),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Sunset/La Brea</h2>
@@ -470,7 +496,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.06914, -118.40604),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>The Boutique</h2>
@@ -480,7 +506,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.07136, -118.40129),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>La Scala</h2>
@@ -490,7 +516,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.09084, -118.38574),
       map: losAngelesMap,
-      icon: nightlife,
+      icon: icons.nightlife.icon,
       book: "evesHollywood",
       content: `
       <h2>Whisky a Go Go</h2>
@@ -500,7 +526,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.05064, -118.24879),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Grand Central Market</h2>
@@ -510,7 +536,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.10877, -118.4469),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>Four Oaks Bar</h2>
@@ -520,7 +546,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.11222, -118.33912),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>The Hollywood Bowl</h2>
@@ -530,7 +556,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.11734, -118.3753),
       map: losAngelesMap,
-      icon: area,
+      icon: icons.area.icon,
       book: "evesHollywood",
       content: `
       <h2>Laurel Canyon</h2>
@@ -540,7 +566,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.08869, -118.3788),
       map: losAngelesMap,
-      icon: hotel,
+      icon: icons.hotel.icon,
       book: "evesHollywood",
       content: `
       <h2>Tropicana Motor Hotel</h2>
@@ -550,7 +576,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.00903, -118.48937),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Santa Monica Civic Auditorium</h2>
@@ -560,7 +586,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.09104, -118.28895),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Hollywood Branch Library</h2>
@@ -570,7 +596,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.1044, -118.34322),
       map: losAngelesMap,
-      icon: hotel,
+      icon: icons.hotel.icon,
       book: "evesHollywood",
       content: `
       <h2>The Landmark Motel</h2>
@@ -580,7 +606,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.058, -118.23738),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>Cielito Lindo (Taquito Place)</h2>
@@ -590,7 +616,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.05713, -118.23981),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Our Lady Queen of Angels Catholic Church</h2>
@@ -600,7 +626,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.09085, -118.37463),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>Barney’s Beanery</h2>
@@ -610,7 +636,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.056, -118.23741),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>Union Station</h2>
@@ -620,7 +646,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(33.93885, -118.24194),
       map: losAngelesMap,
-      icon: landmark,
+      icon: icons.landmark.icon,
       book: "evesHollywood",
       content: `
       <h2>The Watts Towers</h2>
@@ -630,7 +656,7 @@ function initMap() {
     {
       position: new google.maps.LatLng(34.06629, -118.37591),
       map: losAngelesMap,
-      icon: food,
+      icon: icons.food.icon,
       book: "evesHollywood",
       content: `
       <h2>Benihana</h2>
@@ -670,14 +696,25 @@ function initMap() {
     }
   }
 
+  let legend = document.querySelector("#legend");
+  for (let key in icons) {
+    let name = icons[key].name;
+    let icon = icons[key].icon;
+    let div = document.createElement("div");
+    div.setAttribute('class', 'key-item')
+    div.innerHTML = `<img src="${icon}"> ` + name;
+    legend.appendChild(div);
+  }
+  losAngelesMap.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
+
   markers.forEach((marker) => {
     marker.addListener("click", () => {
-      let index = markers.indexOf(marker)
-      for (let i = 0; i < infoWindows.length; i++){
-        if (i === index){
-          infoWindows[i].open(map, markers[i])
+      let index = markers.indexOf(marker);
+      for (let i = 0; i < infoWindows.length; i++) {
+        if (i === index) {
+          infoWindows[i].open(map, markers[i]);
         } else {
-          infoWindows[i].close()
+          infoWindows[i].close();
         }
       }
     });
@@ -685,7 +722,7 @@ function initMap() {
 
   listItems.forEach((item) => {
     item.addEventListener("click", (event) => {
-      infoWindows.forEach(info => info.close())
+      infoWindows.forEach((info) => info.close());
       showMarkers(event.target.id);
     });
   });
